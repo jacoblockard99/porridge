1a2485d73c7dfcf36b973aa6556b7b3030f1c4bf
Jacob <jacoblockard99@gmail.com>
2022-01-16 18:56:54 -0500

    Write Serializer
    Write and minimally test the serializer base class, Serializer.

--- lib/porridge.rb
+++ lib/porridge.rb
@@ -3,6 +3,7 @@
 require_relative 'porridge/version'
 require_relative 'porridge/extractor'
 require_relative 'porridge/send_extractor'
+require_relative 'porridge/serializer'
 
 module Porridge
   class Error < StandardError; end
